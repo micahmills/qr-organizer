59080150c4eba33e9b8f43cd10bf406a659f9445
micahmills <micahmills@gmail.com>
2023-03-20 00:43:41 +0300

    Make Container Name and Location Editable

--- src/ContainerContent.js
+++ src/ContainerContent.js
@@ -1,8 +1,10 @@
-import { html } from 'lit';
+/* eslint-disable no-param-reassign */
+import { html, css } from 'lit';
 import { map } from 'lit/directives/map.js';
 import { ifDefined } from 'lit/directives/if-defined.js';
+import { updateDoc } from 'firebase/firestore';
 import { GenericCard } from './genericCard.js';
-import { db, doc, deleteDoc } from './firebaseConfig.js';
+import { db, doc, deleteDoc, containersRef } from './firebaseConfig.js';
 
 export class ContainerContent extends GenericCard {
   static get properties() {
@@ -14,13 +16,105 @@ export class ContainerContent extends GenericCard {
   }
 
   static get styles() {
-    return [super.styles];
+    return [
+      super.styles,
+      css`
+        header {
+          position: relative;
+        }
+        .editContainerButton {
+          position: absolute;
+          top: 0;
+          right: 0;
+          border: none;
+          background: none;
+          display: flex;
+        }
+        button.iconButton {
+          background: none;
+          border: none;
+        }
+        header [contenteditable='true'] {
+          outline: 1px solid var(--color-dark);
+          outline-offset: 0.5rem;
+        }
+      `,
+    ];
   }
 
   // eslint-disable-next-line class-methods-use-this
   async _deleteItem(id) {
     await deleteDoc(doc(db, 'Items', id));
     this.requestUpdate();
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  async _editItem(id) {
+    console.log(`Edit Item ${id}`);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  async _makeItemEditable(id) {
+    console.log(`Make Item ${id} Editable`);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  _makeContainerEditable(id) {
+    this.shadowRoot
+      .querySelector('.labelNameInput')
+      .setAttribute('contentEditable', 'true');
+    this.shadowRoot
+      .querySelector('.locationInput')
+      .setAttribute('contentEditable', 'true');
+    this.shadowRoot.querySelectorAll('header .label').forEach(label => {
+      label.style.display = 'none';
+    });
+
+    // Init a timeout variable to be used below
+    let timeout = null;
+
+    this.shadowRoot.querySelector('header').addEventListener('input', e => {
+      // Clear the timeout if it has already been set.
+      // This will prevent the previous task from executing
+      // if it has been less than <MILLISECONDS>
+      clearTimeout(timeout);
+
+      let inputType = '';
+      const updatedText = e.target.innerText;
+      if (e.target.classList.contains('labelNameInput')) {
+        inputType = 'labelName';
+      } else {
+        inputType = 'location';
+      }
+
+      // Make a new timeout set to go off in 1000ms (1 second)
+      timeout = setTimeout(() => {
+        this._editContainer(id, inputType, updatedText);
+      }, 1000);
+    });
+  }
+
+  _makeContainerUnEditable() {
+    this.shadowRoot
+      .querySelector('.labelNameInput')
+      .setAttribute('contentEditable', 'false');
+    this.shadowRoot
+      .querySelector('.locationInput')
+      .setAttribute('contentEditable', 'false');
+    this.shadowRoot.querySelectorAll('header .label').forEach(label => {
+      label.style.display = 'initial';
+    });
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  async _editContainer(id, inputType, updatedText) {
+    const containerRef = doc(containersRef, id);
+
+    await updateDoc(containerRef, {
+      [inputType]: updatedText,
+    });
+
+    this._makeContainerUnEditable();
   }
 
   _renderContainerContent() {
@@ -33,17 +127,15 @@ export class ContainerContent extends GenericCard {
               ${item.name}
               <span class="actionButtonsContainer">
                 <button
-                  class=""
-                  @click=${() => {
-                    console.log('edit');
-                  }}
+                  class="iconButton"
+                  @click=${() => this._makeItemEditable(item.id)}
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="16"
                     height="16"
-                    fill="currentColor"
-                    class="bi bi-pencil-fill"
+                    fill="var(--color-light)"
+                    class="pencil"
                     viewBox="0 0 16 16"
                   >
                     <path
@@ -51,13 +143,16 @@ export class ContainerContent extends GenericCard {
                     />
                   </svg>
                 </button>
-                <button class="" @click=${() => this._deleteItem(item.id)}>
+                <button
+                  class="iconButton"
+                  @click=${() => this._deleteItem(item.id)}
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="16"
                     height="16"
-                    fill="currentColor"
-                    class="bi bi-trash3"
+                    fill="var(--color-light)"
+                    class="trash"
                     viewBox="0 0 16 16"
                   >
                     <path
@@ -78,16 +173,44 @@ export class ContainerContent extends GenericCard {
 
   _renderContainerLocation() {
     if (this.container.location) {
-      return html`<h3>Location: ${this.container.location}</h3>`;
+      return html`<h3>
+        <span class="label">Location: </span
+        ><span class="locationInput">${this.container.location}</span>
+      </h3>`;
     }
-    return html``;
+    return html`<h3>
+      <span class="locationInput">${this.container.location}</span>
+    </h3>`;
   }
 
   render() {
     if (this.container) {
       return html`
-        <h2>${ifDefined(this.container.labelName)} Content</h2>
-        ${this._renderContainerLocation()}
+        <header>
+          <h2>
+            <span class="labelNameInput"
+              >${ifDefined(this.container.labelName)}</span
+            ><span class="label"> Content</span>
+          </h2>
+          ${this._renderContainerLocation()}
+          <button
+            class="editContainerButton iconButton"
+            @click=${() => this._makeContainerEditable(this.containerID)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="var(--color-light)"
+              class="pencil"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"
+              />
+            </svg>
+          </button>
+        </header>
         <ul>
           ${this._renderContainerContent()}
         </ul>
